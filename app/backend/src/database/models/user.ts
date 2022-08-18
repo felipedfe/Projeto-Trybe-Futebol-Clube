9345b58731fcb77b04532f5e310555f549a54ff2
Felipe Fernandes <felipedfe@gmail.com>
2022-08-18 12:39:53 -0300

    feat: configuracao do Model de users

---
+++ app/backend/src/database/models/user.ts
@@ -0,0 +1,38 @@
+import { Model, STRING, INTEGER } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  id!: number;
+  username!: string;
+  role!: string;
+  email!: string;
+  password!: string;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: STRING,
+  },
+  role: {
+    type: STRING,
+  },
+  email: {
+    type: STRING,
+  },
+  password: {
+    type: STRING,
+  },
+}, {
+  sequelize: db,
+  modelName: 'user',
+  underscored: true,
+  timestamps: false,
+});
+
+export default User;
